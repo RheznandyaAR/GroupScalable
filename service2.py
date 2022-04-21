0f3327e479440483374d5ba7b2ab73dac7b26cdd
Rheznandya Ardiyan R <81863005+RheznandyaAR@users.noreply.github.com>
2022-04-21 21:21:20 +0700

    initial commit

---
+++ service2.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, jsonify
+
+import requests as r
+
+app = Flask(__name__)
+
+
+@app.route("/recommendation")
+def recommendation():
+    # ambil data dari service 6 (dataserver)
+    data = r.get('http://localhost:5006/products').json()
+
+    try:
+        data = sorted(data[:10], key=lambda k: k['rating'], reverse=True)
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'error'})
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
