e8cfadf21ffd734ae14374084b4819d84051bfff
BlueBeret <63086194+BlueBeret@users.noreply.github.com>
2022-04-21 14:31:05 +0700

    update service 3

--- service3.py
+++ service3.py
@@ -13,11 +13,15 @@ def search():
     # ambil parameter dari request
     search_term = request.args.get('name')
 
+    print(search_term)
     # ambil data dari service 6 (dataserver)
     data = r.get('http://localhost:5006/products').json()
-    
-    matching = [s for s in data if search_term.lower() in s['product_name'].lower()]
 
+    try:
+        matching = [s for s in data if search_term.lower() in s['product_name'].lower()]
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'error'})
     return jsonify(matching)
 
 if __name__ == '__main__':
